093fc6893eff72fe0a5976ad64d041012b939e03
nj-lc <grmnlc@pm.me>
2021-12-28 04:41:20 +0100

    edited userinfo on posts

--- pages/hilfen.js
+++ pages/hilfen.js
@@ -4,7 +4,10 @@ const domain = "http://127.0.0.1:3000"
 export function displayPost(post, mainPost=false) {
   let _style = {};
   if (mainPost) {
-    _style = {"background-color": "#c1117c"};
+    _style = {"background-color": "#272a36"};
+  }
+  if (post.account.acct == "Chizu") {
+    _style = {border: "3px solid #ffd700", ..._style};
   }
   // if (post.visibility != "public")
   //    return (<></>);
@@ -25,8 +28,11 @@ export function displayPost(post, mainPost=false) {
 
   return (
     <div className={styles.post} onClick={() => location.href = `${domain}/notice/${post.id}`} style={_style}>
-      <div className={styles.username}><a href={post.account.url}>{post.account.acct}</a></div>
-      <div className="time">{post.created_at}</div>
+      <div className={styles.userinfo}>
+        <img className={styles.pfp} src={post.account.avatar}/>
+        <div className={styles.username}>{post.account.display_name} <a href={post.account.url}>{"@"+post.account.acct}</a></div>
+        <div className={styles.time}>{/\d{2}:\d{2}:\d{2}/.exec(post.created_at)[0]}</div>
+      </div>
       <br/>
       {media}
       <div className="com" dangerouslySetInnerHTML={{ __html: post.content }}></div>
