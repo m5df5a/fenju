5845485fe2075541084cc0e39046b2691d8f4717
nj-lc <grmnlc@pm.me>
2021-12-28 02:35:13 +0100

    added main post to notice

--- pages/notice/[id].js
+++ pages/notice/[id].js
@@ -5,18 +5,25 @@ import fs from "fs"
 
 export async function getServerSideProps({params}) {
   const token = fs.readFileSync("token.txt", "utf8");
-  const res = await fetch(`https://youjo.love/api/v1/statuses/${params.id}/context`, {headers: {Authorization: token}}).then().catch(x => console.log(x));
+  const res = await fetch(`https://youjo.love/api/v1/statuses/${params.id}/context`, {headers: {Authorization: token}}).then();
   const timeline = await res.json();
-  console.log(timeline)
+  const res_post = await fetch(`https://youjo.love/api/v1/statuses/${params.id}`, {headers: {Authorization: token}}).then();
+  const main_post = await res_post.json();
+  
   return {
     props: {
-      timeline
+      timeline,
+      main_post
     }
   };
 }
 
-export default function notice({timeline}) {
+export default function notice({timeline, main_post}) {
   function make(post) {
+    let _style = {};
+    if (post.id == main_post.id) {
+      _style = {"background-color": "#c1117c"};
+    }
     let media = <></>;
     if (post.media_attachments[0]?.pleroma.mime_type.includes("image")) {
       media = (
@@ -33,7 +40,7 @@ export default function notice({timeline}) {
     }
 
     return (
-      <div className={styles.post} onClick={() => location.href = `http://127.0.0.1:3000/notice/${post.id}`}>
+      <div className={styles.post} onClick={() => location.href = `http://127.0.0.1:3000/notice/${post.id}`} style={_style}>
         <div className="username"><b><a href={post.account.url}>{post.account.acct}</a></b></div>
         <div className="time">{post.created_at}</div>
         <br/>
@@ -45,6 +52,7 @@ export default function notice({timeline}) {
   return (
     <div className={styles.container}>
       {timeline.ancestors.map(post => make(post))}
+      {make(main_post)}
       {timeline.descendants.map(post => make(post))}
     </div>
   )
